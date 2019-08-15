91895a4762c882233d19bcf132be74f2c8f6b0de
Nick Forfinski-Sarkozi <nick.forfinski@gmail.com>
2019-08-15 09:17:48 -0400

    updated pyramiding messaging

--- LAS_Copier_v1.0-beta.py
+++ LAS_Copier_v1.0-beta.py
@@ -2,6 +2,33 @@ import os
 from shutil import copyfile
 import arcpy
 import datetime
+import subprocess
+
+
+def run_console_cmd(cmd, las_tile):
+    process = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    arcpy.AddMessage(output)
+    error_msg = 'ERROR: Failed in getting valid license'
+    error = error.strip().decode('utf-8')
+    if error == error_msg:
+        arcpy.AddWarning('{}|{}'.format(las_tile, error))
+    return output, error
+
+
+def create_las_pyramids(las_tile):
+    exe = r'C:\Program Files\Common Files\LP360\LDPyramid.exe'
+    thin_factor = 12
+    cmd_str = '{} -f {} {}'.format(exe, thin_factor, las_tile)
+    try:
+        output, error = run_console_cmd(cmd_str, las_tile)
+        if output == 'COMPLETE: Finished pyramiding all files.':
+            return True
+        else:
+            return False
+    except Exception as e:
+         arcpy.AddMessage(e)
+         return False
 
 
 def main():
@@ -10,6 +37,7 @@ def main():
 
     dir_from = arcpy.GetParameterAsText(2)
     dir_to = arcpy.GetParameterAsText(3)
+    to_pyramid = arcpy.GetParameter(4)
 
     assigned_tiles = []
     with arcpy.da.SearchCursor(shp, ['reviewer', 'Tile_ID']) as tiles:
@@ -23,6 +51,7 @@ def main():
     arcpy.AddMessage(r"{}, you've got {} tiles assigned to you.".format(reviewer, num_tiles))
 
     num_not_copied = 0
+    no_pyramids = []
 
     arcpy.SetProgressor("step", "Copying LAS files...", 0, num_tiles, 1)
 
@@ -46,6 +75,14 @@ def main():
             copyfile(tile_path_from, tile_path_to)
             arcpy.SetProgressorPosition()
 
+            if to_pyramid:
+                las_status = 'pyramiding {} ({} of {})'.format(las_name, i, num_tiles)
+                arcpy.SetProgressorLabel('{}\n{} ({})'.format(las_status, time_status, mean_dt_status))
+                created_pyramid = create_las_pyramids(tile_path_to)
+
+                if not created_pyramid:
+                    no_pyramids.append(tile_path_to)
+
             if i > 5:
                 mean_dt = (datetime.datetime.now() - tic) / i
                 time_remaining = mean_dt * (num_tiles - i)
@@ -56,7 +93,13 @@ def main():
 
     arcpy.AddMessage('{} of {} las files copied'.format(num_tiles - num_not_copied, num_tiles))
 
+    if no_pyramids:
+        arcpy.AddWarning('Pyramids for the following {} Las tiles were not generated:'.format(len(no_pyramids)))
+        for tile in no_pyramids:
+            arcpy.AddMessage(tile)
+
     arcpy.ResetProgressor()
+
 
 if __name__ == '__main__':
     main()
