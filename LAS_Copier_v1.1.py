4b0df5850d1519041858cf172d2bb8b8f00e565e
Nick Forfinski-Sarkozi <nick.forfinski@gmail.com>
2019-08-15 16:19:28 -0400

    modified LP360 dir logic; added thin factor parameter

--- LAS_Copier_v1.1.py
+++ LAS_Copier_v1.1.py
@@ -6,26 +6,32 @@ import subprocess
 
 
 def run_console_cmd(cmd, las_tile):
-    process = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    process = subprocess.Popen(cmd.split('|'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, error = process.communicate()
-    arcpy.AddMessage(output)
-    error_msg = 'ERROR: Failed in getting valid license'
-    error = error.strip().decode('utf-8')
-    if error == error_msg:
-        arcpy.AddWarning('{}|{}'.format(las_tile, error))
     return output, error
 
 
-def create_las_pyramids(las_tile):
-    exe = r'C:\Program Files\Common Files\LP360\LDPyramid.exe'
-    thin_factor = 12
-    cmd_str = '{} -f {} {}'.format(exe, thin_factor, las_tile)
+def create_las_pyramids(thin_factor, las_tile, lp360_path):
+    exe = r'{}\LDPyramid.exe'.format(lp360_path)
+    cmd_str = '{}|-f|{}|{}'.format(exe, thin_factor, las_tile)
+
     try:
         output, error = run_console_cmd(cmd_str, las_tile)
-        if output == 'COMPLETE: Finished pyramiding all files.':
+
+        error_msg = 'ERROR: Failed in getting valid license'
+        output_msg = 'COMPLETE: Finished pyramiding all files.'
+
+        error = error.strip().decode('utf-8')
+        output = output.strip().decode('utf-8')
+
+        #arcpy.AddMessage(output)
+        arcpy.AddMessage(error)
+
+        if output == output_msg:
             return True
         else:
             return False
+
     except Exception as e:
          arcpy.AddMessage(e)
          return False
@@ -38,6 +44,8 @@ def main():
     dir_from = arcpy.GetParameterAsText(2)
     dir_to = arcpy.GetParameterAsText(3)
     to_pyramid = arcpy.GetParameter(4)
+    thin_factor = arcpy.GetParameter(5)
+    lp360_path = arcpy.GetParameterAsText(6)
 
     assigned_tiles = []
     with arcpy.da.SearchCursor(shp, ['reviewer', 'Tile_ID']) as tiles:
@@ -46,12 +54,15 @@ def main():
                 assigned_tiles.append(tile)
 
     num_tiles = len(assigned_tiles)
+    num_pyramids = 0
 
     arcpy.AddMessage(shp)
     arcpy.AddMessage(r"{}, you've got {} tiles assigned to you.".format(reviewer, num_tiles))
 
+    num_copied_tiles = 0
     num_not_copied = 0
-    no_pyramids = []
+
+    tiles_not_pyramided = []
 
     arcpy.SetProgressor("step", "Copying LAS files...", 0, num_tiles, 1)
 
@@ -73,15 +84,17 @@ def main():
 
             arcpy.SetProgressorLabel('{}\n{} ({})'.format(las_status, time_status, mean_dt_status))
             copyfile(tile_path_from, tile_path_to)
+            num_copied_tiles += 1
             arcpy.SetProgressorPosition()
 
             if to_pyramid:
                 las_status = 'pyramiding {} ({} of {})'.format(las_name, i, num_tiles)
                 arcpy.SetProgressorLabel('{}\n{} ({})'.format(las_status, time_status, mean_dt_status))
-                created_pyramid = create_las_pyramids(tile_path_to)
+                created_pyramid = create_las_pyramids(thin_factor, tile_path_to, lp360_path)
+                num_pyramids += 1
 
                 if not created_pyramid:
-                    no_pyramids.append(tile_path_to)
+                    tiles_not_pyramided.append(tile_path_to)
 
             if i > 5:
                 mean_dt = (datetime.datetime.now() - tic) / i
@@ -91,12 +104,16 @@ def main():
             arcpy.AddError(e)
             num_not_copied += 1
 
-    arcpy.AddMessage('{} of {} las files copied'.format(num_tiles - num_not_copied, num_tiles))
+    arcpy.AddMessage('{} Summary {}'.format('-' * 20, '-' * 20))
+    arcpy.AddMessage('{} of {} las files copied'.format(num_copied_tiles, num_tiles))
 
-    if no_pyramids:
-        arcpy.AddWarning('Pyramids for the following {} Las tiles were not generated:'.format(len(no_pyramids)))
-        for tile in no_pyramids:
-            arcpy.AddMessage(tile)
+    if to_pyramid:
+        arcpy.AddMessage('{} tiles pyramided'.format(num_pyramids))    
+
+        if not tiles_not_pyramided:
+            arcpy.AddWarning('Pyramids for the following {} Las tiles were not generated:'.format(len(tiles_not_pyramided)))
+            for tile in tiles_not_pyramided:
+                arcpy.AddMessage(tile)
 
     arcpy.ResetProgressor()
 
