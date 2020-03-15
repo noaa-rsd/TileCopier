23f5c8215e334ea304a124d71e4b3ef7adfb2fea
Nick Forfinski-Sarkozi <nick.forfinski@gmail.com>
2020-03-14 23:01:28 -0400

    refactored to OOP with TileCopier.py

--- LAS_Copier_v1.1.py
+++ LAS_Copier_v1.1.py
@@ -3,118 +3,157 @@ from shutil import copyfile
 import arcpy
 import datetime
 import subprocess
+import collections
 
 
-def run_console_cmd(cmd, las_tile):
-    process = subprocess.Popen(cmd.split('|'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    output, error = process.communicate()
-    return output, error
+class TileCopier:
 
+    def __init__(self):
+        self.shp = arcpy.GetParameter(0)
+        self.reviewer = arcpy.GetParameter(1)
+        self.las_from = arcpy.GetParameterAsText(2)
+        self.las_to = arcpy.GetParameterAsText(3)
+        self.to_pyramid = arcpy.GetParameter(4)
+        self.thin_factor = arcpy.GetParameter(5)
+        self.lp360_path = arcpy.GetParameterAsText(6)
+        self.dem_from = arcpy.GetParameterAsText(7)
+        self.dem_to = arcpy.GetParameterAsText(8)
 
-def create_las_pyramids(thin_factor, las_tile, lp360_path):
-    exe = r'{}\LDPyramid.exe'.format(lp360_path)
-    cmd_str = '{}|-f|{}|{}'.format(exe, thin_factor, las_tile)
+        self.shp_srs = arcpy.Describe(self.shp).spatialReference
+        self.export_tile_fields = collections.OrderedDict([
+            ('reviewer', 'TEXT'), 
+            ('tile_id', 'TEXT'), 
+            ('las_name', 'TEXT'), 
+            ('las_from', 'TEXT'), 
+            ('las_to', 'TEXT'), 
+            ('las_copied', 'TEXT'), 
+            ('pyramid', 'TEXT'),
+            ('dem_name', 'TEXT'), 
+            ('dem_from', 'TEXT'), 
+            ('dem_to', 'TEXT'), 
+            ('dem_copied', 'TEXT')
+            ])
+        self.tile = None
+        self.created_pyramid = False
+        self.las_copied = False  # default, change if copied
+        self.dem_copied = False  # default, change if copied
 
-    try:
-        output, error = run_console_cmd(cmd_str, las_tile)
+    def set_current_tile(self, tile, i):
+        self.oid = i
+        self.tile_geom = tile[0]
+        self.reviewer = tile[1]
+        self.tile_id = tile[2]
+        self.las_name = tile[3] + '.las'
+        self.dem_name = tile[4] + '.img'
 
-        error_msg = 'ERROR: Failed in getting valid license'
-        output_msg = 'COMPLETE: Finished pyramiding all files.'
+    def get_tiles(self):
+        assigned_tiles = []
+        fields = ['SHAPE@WKT', 'reviewer', 'Tile_ID', 'LAS_Name', 'DEM_Name']
+        with arcpy.da.SearchCursor(self.shp, fields) as tiles:
+            for tile in tiles:
+                if tile[1] == self.reviewer:
+                    assigned_tiles.append(tile)
 
-        error = error.strip().decode('utf-8')
-        output = output.strip().decode('utf-8')
+        num_tiles = len(assigned_tiles)
+        arcpy.AddMessage(r"{}, you've got {} tiles assigned to you.".format(self.reviewer, num_tiles))
+        return assigned_tiles, num_tiles
 
-        #arcpy.AddMessage(output)
-        arcpy.AddMessage(error)
+    def create_copied_tile_status_shp(self):
+        shp_processed_dir = os.path.dirname(self.shp.value)
+        shp_processed_name = os.path.basename(self.shp.value).replace('.shp', '_TileCopierResults.shp')
+        arcpy.CreateFeatureclass_management(shp_processed_dir, 
+                                            shp_processed_name,
+                                            spatial_reference=self.shp_srs)
 
-        if output == output_msg:
-            return True
-        else:
-            return False
+        shp_processed_path = os.path.join(shp_processed_dir, shp_processed_name)
+        for field, dtype in self.export_tile_fields.items():
+            arcpy.AddField_management(shp_processed_path, field, dtype)
 
-    except Exception as e:
-         arcpy.AddMessage(e)
-         return False
+        insert_cursor_fields = ['SHAPE@', 'OID@'] + list(self.export_tile_fields.keys())
+        self.tile_cursor = arcpy.da.InsertCursor(shp_processed_path, insert_cursor_fields)
+
+    @staticmethod
+    def run_console_cmd(cmd, las_tile):
+        process = subprocess.Popen(cmd.split('|'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+        return output, error
+    
+    @staticmethod
+    def create_las_pyramids(thin_factor, las_tile, lp360_path):
+        exe = r'{}\LDPyramid.exe'.format(lp360_path)
+        cmd_str = '{}|-f|{}|{}'.format(exe, thin_factor, las_tile)
+
+        try:
+            output, error = run_console_cmd(cmd_str, las_tile)
+            error_msg = 'ERROR: Failed in getting valid license'
+            output_msg = 'COMPLETE: Finished pyramiding all files.'
+            error = error.strip().decode('utf-8')
+            output = output.strip().decode('utf-8')
+
+            #arcpy.AddMessage(output)
+            arcpy.AddMessage(error)
+
+            if output == output_msg:
+                return True
+            else:
+                return False
+
+        except Exception as e:
+             arcpy.AddMessage(e)
+             return False
+ 
+    def copy_las(self):
+        las_path_from = os.path.join(self.las_from, self.las_name)
+        las_path_to = os.path.join(self.las_to, self.las_name)
+        try:
+            if self.las_from and self.las_to:
+                copyfile(las_path_from, las_path_to)
+                self.las_copied = True
+                if self.to_pyramid:
+                    tile_status = 'pyramiding {} ({} of {})'.format(las_name, i, num_tiles)
+                    arcpy.SetProgressorLabel(tile_status)
+                    self.created_pyramid = create_las_pyramids(thin_factor, las_path_to, lp360_path)
+        except Exception as e:
+            arcpy.AddError(e)
+
+    def copy_dem(self):
+        dem_path_from = os.path.join(self.dem_from, self.dem_name)
+        dem_path_to = os.path.join(self.dem_to, self.dem_name)
+        try:
+            if self.dem_from and self.dem_to:
+                copyfile(dem_path_from, dem_path_to)
+                self.dem_copied = True
+        except Exception as e:
+            arcpy.AddError(e)
+
+    def update_status_shp(self):
+        tile_data = (arcpy.FromWKT(self.tile_geom, self.shp_srs), 
+                     self.oid, self.reviewer, self.tile_id, 
+                     self.las_name, self.las_from, self.las_to, 
+                     str(self.las_copied), str(self.created_pyramid), 
+                     self.dem_name, self.dem_from, self.dem_to, 
+                     str(self.dem_copied))
+        self.tile_cursor.insertRow(tile_data)
 
 
 def main():
-    shp = arcpy.GetParameter(0)
-    reviewer = arcpy.GetParameter(1)
-
-    dir_from = arcpy.GetParameterAsText(2)
-    dir_to = arcpy.GetParameterAsText(3)
-    to_pyramid = arcpy.GetParameter(4)
-    thin_factor = arcpy.GetParameter(5)
-    lp360_path = arcpy.GetParameterAsText(6)
-
-    assigned_tiles = []
-    with arcpy.da.SearchCursor(shp, ['reviewer', 'Tile_ID']) as tiles:
-        for tile in tiles:
-            if tile[0] == reviewer:
-                assigned_tiles.append(tile)
-
-    num_tiles = len(assigned_tiles)
-    num_pyramids = 0
-
-    arcpy.AddMessage(shp)
-    arcpy.AddMessage(r"{}, you've got {} tiles assigned to you.".format(reviewer, num_tiles))
-
-    num_copied_tiles = 0
-    num_not_copied = 0
-
-    tiles_not_pyramided = []
-
-    arcpy.SetProgressor("step", "Copying LAS files...", 0, num_tiles, 1)
-
-    mean_dt = '?'
-    time_remaining = 'calculating initial guess...'
-    las_status = None
-    time_status = None
-    tic = datetime.datetime.now()
+    tile_copier = TileCopier()
+    assigned_tiles, num_tiles = tile_copier.get_tiles()
+    tile_copier.create_copied_tile_status_shp()
+    arcpy.SetProgressor("step", "Copying tile data...", 0, num_tiles, 1)
 
     for i, tile in enumerate(assigned_tiles, 1):
-        las_name = tile[1] + '.las'
-        tile_path_from = os.path.join(dir_from, las_name)
-        tile_path_to = os.path.join(dir_to, las_name)
-        
-        try:
-            las_status = 'copying {} ({} of {})'.format(las_name, i, num_tiles)
-            time_status = 'Est. Time Remaining: {}'.format(str(time_remaining).split('.')[0])
-            mean_dt_status = 'Mean Time per LAS: {}'.format(str(mean_dt).split('.')[0])
+        tile_copier.set_current_tile(tile, i)
+        tile_status = 'tile {} ({} of {})'.format(tile_copier.tile_id, i, num_tiles)
+        arcpy.SetProgressorLabel(tile_status)
 
-            arcpy.SetProgressorLabel('{}\n{} ({})'.format(las_status, time_status, mean_dt_status))
-            copyfile(tile_path_from, tile_path_to)
-            num_copied_tiles += 1
-            arcpy.SetProgressorPosition()
+        tile_copier.copy_las()
+        tile_copier.copy_dem()
 
-            if to_pyramid:
-                las_status = 'pyramiding {} ({} of {})'.format(las_name, i, num_tiles)
-                arcpy.SetProgressorLabel('{}\n{} ({})'.format(las_status, time_status, mean_dt_status))
-                created_pyramid = create_las_pyramids(thin_factor, tile_path_to, lp360_path)
-                num_pyramids += 1
+        arcpy.SetProgressorPosition()
+        tile_copier.update_status_shp()
 
-                if not created_pyramid:
-                    tiles_not_pyramided.append(tile_path_to)
-
-            if i > 5:
-                mean_dt = (datetime.datetime.now() - tic) / i
-                time_remaining = mean_dt * (num_tiles - i)
-
-        except Exception as e:
-            arcpy.AddError(e)
-            num_not_copied += 1
-
-    arcpy.AddMessage('{} Summary {}'.format('-' * 20, '-' * 20))
-    arcpy.AddMessage('{} of {} las files copied'.format(num_copied_tiles, num_tiles))
-
-    if to_pyramid:
-        arcpy.AddMessage('{} tiles pyramided'.format(num_pyramids))    
-
-        if not tiles_not_pyramided:
-            arcpy.AddWarning('Pyramids for the following {} Las tiles were not generated:'.format(len(tiles_not_pyramided)))
-            for tile in tiles_not_pyramided:
-                arcpy.AddMessage(tile)
-
+    del tile_copier.tile_cursor
     arcpy.ResetProgressor()
 
 
